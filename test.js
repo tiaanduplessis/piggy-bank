7a640ba316c5b1863c8db25b39b59208cb3e74cd
Tiaan <tiaanduplessis@hotmail.com>
2017-07-11 18:10:46 +0200

    Write basic tests

--- test.js
+++ test.js
@@ -1,2 +1,27 @@
 /* eslint-env jest */
 'use strict'
+
+const del = require('del')
+const createStore = require('./index')
+
+let store
+
+beforeEach(() => {
+  store = createStore()
+})
+
+test('should create a store', () => {
+  expect(createStore).toBeDefined()
+  expect(typeof createStore).toBe('function')
+  expect(store).toBeDefined()
+  expect(typeof store).toBe('object')
+})
+
+test('should set and get', () => {
+  store.set('foo', 1)
+  expect(store.get('foo')).toBe(1)
+})
+
+afterEach(() => {
+  del('piggyBank.json')
+})
